a6f6c33f1502a65b058486bd8971a1f96e081eaf
jas2063084 <jas2063084@maricopa.edu>
2015-12-10 22:02:55 -0700

    Update get2prolog.c
    reverting back to char, to see if it works in unix and without crashing. tested, does not work in windows.

--- src/get2prolog.c
+++ src/get2prolog.c
@@ -1,6 +1,5 @@
-#include <stdio.h>
+#include<stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 //     INPUT: 
 //            string predicate, argument1, argument2, argument3
@@ -14,9 +13,6 @@ int main () {
   const char *mode = "r";
   char outFilename[] = "output.csv";
   char ch;
-  //for customization we could make this an arg ([72]) so we could manually set this value at start
-  char byLine[72];
-  //char temp[72];
   
   input_file = fopen("input.csv", mode);
   
@@ -33,58 +29,52 @@ int main () {
     exit(1);
   }
   printf("Prolog format output of %s file are :\n", outFilename);
-  
-  //control variables and psuedo constants
+   //control variables for while loop and char swap/insert
   int commaCount = 0;
   char par = ')';
   char period = '.';
-  char * paranPeriod = ").";
-  char control = '\n';
-  int carReturn = '\r';
+  int commasPerLine = 0;
   
-  //beginning of the work in a loop to go through all lines of the file
-  while (fgets(byLine,sizeof(byLine),input_file) != NULL){
-	
-	//iteration control for reading each character in a string
-	char *ptr = byLine;
-	//these create a values for each line read to be used with string copy
-	int adjustedLength = strlen(byLine) + strlen(paranPeriod);
-	int testLength = strlen(byLine);
-	int insertLocation = testLength - 1;
-	
-	//does the work to place the first paran, may need some optimization. was also going to be used
-	//for total comma count for other purposes. works so left it this way
-	while (*ptr != '\0'){
-		if (*ptr == ',' && commaCount == 0){
-			*ptr = '(';
+  //loop print out format and writes to file
+  while( ( ch = fgetc(input_file) ) != EOF ){
+		
+		//test for first comma, its understood file supplied is line by line and has been confirmed
+		if (ch == ',' && commaCount == 0 ){
+			ch = '(';
 			commaCount++;
 		}
-		ptr++;
-	}
-	
-	//temp string used to store string copy of final product to be output
-	char* output = (char*)malloc(adjustedLength);
-	
-	strncpy(output, byLine, insertLocation);
-	output[insertLocation] = '\0';
-	strcat(output, paranPeriod);
-	strcat(output, byLine + insertLocation);
-	
-	//write to screen and file
-	printf("%s", output);
-	fputs(output, out_file);
-	
-	//reset comma count for next line to make first paran
-	commaCount = 0;
-	//clearing the memory of the output string, since this is in a loop even more important
-	free(output);
+		//counts to keep track of commas in a line
+		else{
+			if(ch == ',' && commaCount > 0)
+				commaCount++;
+		}
+							
+		//information on what file will look like
+		if(ch == '\n'){
+			printf("%c",par);
+			printf("%c",period);
+		}
+		printf("%c",ch);
+		
+		//writing the new prolog file
+		if(ch == '\n'){
+			fputc(par, out_file);
+			fputc(period, out_file);
+			//resets for comma control, placing first paran
+			commaCount = 0;
+		}
+		fputc(ch, out_file);
+		
+		
   }
-  //covers end of file to add the paran and period
-  //printf("%c",par);
-  //printf("%c", period);
-  //fputc(par, out_file);
-  //fputc(period, out_file);
-
+/*
+  //finishes the last display character since it is not read
+  printf("%c",par);
+  printf("%c", period);
+  //writes the last character since it is not read
+  fputc(par, out_file);
+  fputc(period, out_file);
+*/
   /* close the file */
   fclose(input_file);
   fclose(out_file);
